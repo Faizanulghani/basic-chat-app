f6023cbea5f829154b0fcff00320e727d216cdbc
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-11 18:35:09 +0500

    Added socket.io

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -8,7 +9,17 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile("./public/index.html");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
